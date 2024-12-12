4bf09cbb0b82db79a116cc25848e7bf71b190059
Maurice Wilhelm <maurice.wilhelm@mt-ag.com>
2024-12-12 13:23:09 +0100

    Fix formatting time duration in job views

--- src/plsql/vw/qa_job_run_details_p0009_v.sql
+++ src/plsql/vw/qa_job_run_details_p0009_v.sql
@@ -1,16 +1,16 @@
 create or replace force view qa_job_run_details_p0009_v as
 select job_name
       ,log_date
-      ,case when status = 'FAILED' then 'ERROR' else status end status
+      ,case when status = 'FAILED' then 'ERROR' else status end as status
       ,error#
       ,errors
-      ,lpad(to_char(extract(hour from run_duration))
-           ,2
-           ,'0') || ':' || lpad(to_char(extract(minute from run_duration))
-                               ,2
-                               ,'0') || ':' || lpad(to_char(extract(second from run_duration))
-                                                   ,2
-                                                   ,'0') as run_duration
+      ,nvl(lpad(to_char(extract(hour from run_duration))
+          ,2
+          ,'0'), '00') || ':' || nvl(lpad(to_char(extract(minute from run_duration))
+                                    ,2
+                                    ,'0'), '00') || ':' || nvl(lpad(to_char(floor(extract(second from run_duration)))
+                                                              ,2
+                                                              ,'0'), '00') as run_duration
 from user_scheduler_job_run_details
 where job_name = qa_utils_pkg.f_get_constant_string_value('gc_utplsql_scheduler_cronjob_name')
 order by log_date desc;
